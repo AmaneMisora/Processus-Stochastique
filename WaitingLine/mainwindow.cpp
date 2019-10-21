1e795d9d9d357ae52200b15ca5ec44be5e26f8ea
Lucie Vallaude <lucie.vallade19@gmail.com>
2019-10-21 22:04:19 +0200

    Modification de l'interface

--- WaitingLine/mainwindow.cpp
+++ WaitingLine/mainwindow.cpp
@@ -7,15 +7,28 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    // init GroupBoxProblem
+// init GroupBoxProblem
+
+    // Question
+    ui->ComboBoxQuestion->addItem(" ");
     ui->ComboBoxQuestion->addItem("Combien de client en moyenne seront présents?");
     ui->ComboBoxQuestion->addItem("Combien de temps en moyenne le client sera présent?");
     ui->ComboBoxQuestion->addItem("Combien de client seront perdus?");
     ui->ComboBoxQuestion->addItem("Quelle est la probilité d'avoir ce nombre de clients dans la file?");
     ui->ComboBoxQuestion->addItem("Quelle est la probilité qu'un client attende ce temps là?");
 
+    // ComboBox (Q4&5)
+    ui->ComboBoxQ4->addItem("plus de");
+    ui->ComboBoxQ4->addItem("moins de");
+    ui->ComboBoxQ4->addItem("entre");
+    ui->ComboBoxQ5->addItem("plus de");
+    ui->ComboBoxQ5->addItem("moins de");
+    ui->ComboBoxQ5->addItem("entre");
+
+    // GroupBox
     ui->GroupBox3->hide();
-    ui->GroupBox45->hide();
+    ui->GroupBox4->hide();
+    ui->GroupBox5->hide();
 }
 
 MainWindow::~MainWindow()
@@ -25,24 +38,52 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_ComboBoxQuestion_currentIndexChanged(int index)
 {
-    if(index == 0 || index == 1)
+    if(index == 0)
+    {
+        ui->GroupBox12->hide();
+        ui->GroupBox3->hide();
+        ui->GroupBox4->hide();
+        ui->GroupBox5->hide();
+    }
+
+    if(index == 1 || index == 2)
     {
         ui->GroupBox12->show();
         ui->GroupBox3->hide();
-        ui->GroupBox45->hide();
+        ui->GroupBox4->hide();
+        ui->GroupBox5->hide();
+
+        ui->RadioButtonQueue->click();
     }
 
-    if(index == 2)
+    if(index == 3)
     {
         ui->GroupBox12->hide();
         ui->GroupBox3->show();
-        ui->GroupBox45->hide();
+        ui->GroupBox4->hide();
+        ui->GroupBox5->hide();
     }
 
-    if(index == 3 || index == 4)
+    if(index == 4)
     {
         ui->GroupBox12->hide();
         ui->GroupBox3->hide();
-        ui->GroupBox45->show();
+        ui->GroupBox4->show();
+        ui->GroupBox5->hide();
+
+        ui->LabelQ4->hide();
+        ui->SpinBoxQ4_2->hide();
+    }
+
+    if(index == 5)
+    {
+        ui->GroupBox12->hide();
+        ui->GroupBox3->hide();
+        ui->GroupBox4->hide();
+        ui->GroupBox5->show();
+
+        ui->LabelQ5->hide();
+        ui->SpinBoxQ5_2->hide();
+        ui->ComboBoxTimeQ5_2->hide();
     }
 }
